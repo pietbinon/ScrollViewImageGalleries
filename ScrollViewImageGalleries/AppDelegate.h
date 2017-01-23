1cc906725567e9779f1fc80b457c57c8566ae41d
pietbinon <pierrebinon@icloud.com>
2017-01-23 14:01:38 -0500

    Initial Commit

---
+++ ScrollViewImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleries
+//
+//  Created by Pierre Binon on 2017-01-23.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Pierre Binon on 2017-01-23.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
