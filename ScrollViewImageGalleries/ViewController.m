b7c3611d2565028ed57867f093c512dd4bd74c98
pietbinon <pierrebinon@icloud.com>
2017-01-23 16:25:54 -0500

    First commit Jan 23

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -8,15 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+
+
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) NSArray<UIImage*>* lighthouseImages;
 
 @end
+
+
+
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.lighthouseImages = @[[UIImage imageNamed:@"Lighthouse-in-Field"],
+                         [UIImage imageNamed:@"Lighthouse-night"],
+                         [UIImage imageNamed:@"Lighthouse"]];
+}
+
+
+- (void) viewWillAppear:(BOOL)animated {
+    [self setUpScrollViewForLighthouse];
+}
+
+
+- (void)setUpScrollViewForLighthouse {
+    CGRect viewRect = self.view.bounds;
+    CGFloat imageX = 0;
+    // Make an uiimageview for each phone image
+    for (UIImage *lighthouseImage in self.lighthouseImages) {
+        // Create image view with image
+        UIImageView *imgView = [[UIImageView alloc] initWithImage:lighthouseImage];
+        // Set it to scale reasonably so images don't get distorted
+        imgView.contentMode = UIViewContentModeScaleAspectFit;
+        // Frame = size of the view, moved 0 on x-axis & imageY on y-axis
+        CGRect imgFrame = CGRectOffset(viewRect, imageX, 0);
+        imgView.frame = imgFrame;
+        // Increase imageY by the height of one view, so next view is below this one
+        imageX += CGRectGetWidth(viewRect);
+        // Add each as a subview of the scrollview
+        [self.scrollView addSubview:imgView];
+    }
+    // Make scrollview move in discrete pages
+    self.scrollView.pagingEnabled = YES;
+    // Set the content size of the scrollview
+    self.scrollView.contentSize = CGSizeMake(CGRectGetHeight(viewRect) * self.lighthouseImages.count,
+                                             CGRectGetWidth(viewRect));
+    
+    [self.scrollView setTranslatesAutoresizingMaskIntoConstraints: YES];
 }
 
 
@@ -24,6 +70,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
